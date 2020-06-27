ef915f00d8c176bbae3f0b6aba1a47f2490589f1
Praveen Kumar <pravkuma@gmail.com>
2020-06-27 20:58:26 +0530

    First commit in java code

---
+++ src/main/java/com/codegen/core/plateform/coreapplication/CoreapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.codegen.core.plateform.coreapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreapplicationApplication.class, args);
+	}
+
+}
